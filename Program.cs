be5e02639fd4cdaabbaee1b54b83de1b6dde770c
Carlos Diaz <carlosdiaz@Carloss-Mac-mini.local>
2025-09-10 20:28:45 -0400

    In this commit I added an abstract method Prepare in the ingridient class that is implement in each of the ingridients classes.

--- Program.cs
+++ Program.cs
@@ -13,12 +13,12 @@
 // Ingredient ingredient = new Cheddar();
 // System.Console.WriteLine(ingredient.Name);
 
-var ingredients = new List<Ingredient>
-{
-    new Cheddar(),
-    new Mozzarella(),
-    new TomatoSauce()
-};
+// var ingredients = new List<Ingredient>
+// {
+//     new Cheddar(),
+//     new Mozzarella(),
+//     new TomatoSauce()
+// };
 
 foreach(var ingredient in ingredients)
 {
@@ -43,33 +43,66 @@ public class Pizza
 
 public class Ingredient
 {
+    public Ingredient(int priceIfExtraTopping)
+    {
+        System.Console.WriteLine("Constructor from th Ingredient class");
+        PriceIfExtraTopping = priceIfExtraTopping;
+    }
+    public int PriceIfExtraTopping { get; }
     public virtual string Name { get; } = "Some ingredient";
+    public abstract void Prepare();
     public int PublicField;
 
     public string PublicMethod() => "This method is PUBLIC in the Ingriedient class.";
     private string PrivateMethod() => "This method is PRIVATE in the Ingriedient class.";
 }
 
-public class Cheese : Ingredient
+public abstract class Cheese : Ingredient
 {
-    
+    public Cheese(int priceIfExtraTopping) : base(priceIfExtraTopping)
+    {
+    }
+
+
 }
 
 public class Cheddar : Cheese
 {
+    public Cheddar(int priceIfExtraTopping) : base(priceIfExtraTopping)
+    {
+    }
     public override string Name => "Cheddar cheese";
     public int AgedForMonths { get; }
-
+    public override void Prepare() => Console.WriteLine("Grate and sprinkle over pizza.");
 }
 
 public class TomatoSauce : Ingredient
 {
+    public TomatoSauce(int priceIfExtraTopping) : base(priceIfExtraTopping)
+    {
+    }
     public override string Name => "Tomato sauce";
     public bool TomatoesIn100Grams { get; }
+    public override void Prepare() => Console.WriteLine("Cook tomatoes with basil, garlic and salt. " + 
+        "Spread on pizza.");
+
 }
 
 public class Mozzarella : Cheese
 {
+    public Mozzarella(int priceIfExtraTopping) : base(priceIfExtraTopping)
+    {
+    }
     public override string Name => "Mozzarella cheese";
     public bool IsLight { get; }
+    public override void Prepare() => Console.WriteLine("Slice thinly and place on top of the pizza.");
 }
+
+// Ingredient GenerateRandomIngredient()
+// {
+//     var random = new Random();
+//     var number = random.Next(1, 4);
+//     if (number == 1) { return new Cheddar(2, 12); }
+//     if (number == 2) { return new TomatoSauce(true); }
+//     else { return new Mozzarella(2); }
+// }
